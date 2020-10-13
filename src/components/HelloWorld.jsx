09f7c1e0a6e4b603178fe42d0cec694e4df0b1be
chriszdeux <chriszdeux@gmail.com>
2020-10-13 12:25:09 -0700

    Eslint

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+const HelloWorld = () => (
+    <div>
+        <h1>Hola mundo</h1>
+        <h2>hi</h2>
+    </div>
+)
+
+export default HelloWorld;
